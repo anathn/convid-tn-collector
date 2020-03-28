8b24d6e15c1f41c3d7c3de3e04f2b0eda1f3cb48
Nathan Sloan <nathan@nathansloan.com>
2020-03-27 21:18:38 -0400

    catching int parse errors

--- get_convid_tn.py
+++ get_convid_tn.py
@@ -17,7 +17,12 @@ for line in body:
     county = cell[1][cell[1].find("<p>")+3:cell[1].find("</p>")]
     num = cell[2][cell[2].find("<p>")+3:cell[2].find("</p>")]
     timestampStr = datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)")
-    total += int(num)
+    try:
+        total += int(num)
+    except:
+        # We just display this - who cares if it fails
+        print("Error converging {0} to int".format(num))
+    
     output =  '"{0}", "{1}", {2}\n'.format(timestampStr, county, num)
 
     print(output)
